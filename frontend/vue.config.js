93d2915e3e35535784652aa75434f2792972dde4
Michal Repec <mikerepec@gmail.com>
2020-10-04 10:24:58 +0200

    Setup local dev server proxy to avoid cors

---
+++ frontend/vue.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  devServer: {
+    proxy: {
+      '^/api': {
+        target: 'http://localhost:9000',
+        changeOrigin: true,
+        pathRewrite: {
+          '^/api/': '',
+        },
+      },
+    },
+  },
+};
