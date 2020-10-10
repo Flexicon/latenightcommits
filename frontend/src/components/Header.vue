61af335e9997ed28bd519288f919b4d3966c5e2c
Michal Repec <mikerepec@gmail.com>
2020-10-10 17:59:38 +0200

    Update tagline

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -3,7 +3,7 @@
     <div class="m-auto py-5 px-4 mb-2 font-serif lg:max-w-4xl lg:px-0">
       <h1 class="text-2xl sm:text-5xl">🍺 Late Night Commits</h1>
       <p class="text-sm sm:text-base text-gray-600">
-        Cause nothing good ever happens after 2 A.M.
+        When the pressures of being 10x just overwhelm you
       </p>
     </div>
   </div>
