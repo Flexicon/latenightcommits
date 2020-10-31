32b953079d59a7417a51cf5fba1db52501c23cb9
Michal Repec <mikerepec@gmail.com>
2020-10-31 14:01:34 +0100

    Add inspiration link

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -6,5 +6,13 @@
         When the pressures of being 10x just overwhelm you
       </p>
     </div>
+    <a
+      class="block absolute right-0 top-0 py-1 px-2 text-2xs text-gray-500 bg-gray-200 hover:underline hover:text-gray-700 hover:bg-gray-300"
+      href="http://www.commitlogsfromlastnight.com/"
+      rel="noreferrer noopener"
+      target="_blank"
+    >
+      inspiration
+    </a>
   </div>
 </template>
