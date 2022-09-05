d320a61d8bc2b3519a85a64d960a9c89bcc83917
Mike Repec <mikerepec@gmail.com>
2022-09-05 17:43:39 +0200

    Schedule fetch_worker on a cron

--- fetch_worker.go
+++ fetch_worker.go
@@ -3,13 +3,24 @@ package main
 import (
 	"log"
 
+	"github.com/pkg/errors"
+	"github.com/robfig/cron/v3"
 	"gorm.io/gorm"
 )
 
 func runFetchWorker(db *gorm.DB, api *GitHubAPI) error {
-	log.Println("Running fetch_worker...")
+	log.Println("Running fetch_worker")
+	scheduler := cron.New()
 
-	// TODO: implement
+	_, err := scheduler.AddFunc("*/10 * * * *", func() {
+		if err := runFetchJob(db, api); err != nil {
+			log.Fatalln(err)
+		}
+	})
+	if err != nil {
+		return errors.Wrap(err, "failed to schedule fetch job worker")
+	}
 
+	scheduler.Run()
 	return nil
 }
