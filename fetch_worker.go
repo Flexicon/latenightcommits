328db49da4c103e5c010f3147b37cdf0e5ea2a1a
Mike Repeć <mikerepec@gmail.com>
2022-09-07 18:32:33 +0200

    Fetch worker (#3)
    * Add fetch_worker docker file
    
    * Expose http liveness check from fetch worker

--- fetch_worker.go
+++ fetch_worker.go
@@ -1,23 +1,38 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"net/http"
+	"time"
 
 	"github.com/pkg/errors"
 	"github.com/robfig/cron/v3"
+	"github.com/spf13/viper"
 	"gorm.io/gorm"
 )
 
 func runFetchWorker(db *gorm.DB, api *GitHubAPI) error {
 	log.Println("Running fetch_worker")
+	start := time.Now()
+
+	// Expose simple http liveness check
+	go func() {
+		http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+			fmt.Fprintf(w, "Late Night Commits fetch worker running since: %v", start)
+		})
+
+		log.Fatalln(http.ListenAndServe(":"+viper.GetString("port"), nil))
+	}()
+
+	// Run fetch job on a schedule
 	scheduler := cron.New()
 
-	_, err := scheduler.AddFunc("*/10 * * * *", func() {
+	if _, err := scheduler.AddFunc("*/10 * * * *", func() {
 		if err := runFetchJob(db, api); err != nil {
 			log.Printf("Fetch job error: %v", err)
 		}
-	})
-	if err != nil {
+	}); err != nil {
 		return errors.Wrap(err, "failed to schedule fetch job worker")
 	}
 
